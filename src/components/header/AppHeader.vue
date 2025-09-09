75566d951174db6338fd530541a3b9a198cc2169
leonardo029 <moreira.leonardo83@outlook.com>
2025-09-09 10:48:55 -0300

    feat: Criação do Header da aplicação

---
+++ src/components/header/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar class="pa-1 pb-2" :elevation="24" color="red-accent-4" elevate>
+    <AppLogo/>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+export default {
+  name: "AppHeader"
+}
+</script>
