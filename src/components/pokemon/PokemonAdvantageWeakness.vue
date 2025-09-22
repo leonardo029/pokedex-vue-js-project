970a2725123c95a1bacc57b711235a81d83a44c0
leonardo029 <moreira.leonardo83@outlook.com>
2025-09-22 11:06:36 -0300

    feat: feat: Tratamento visual para a inexistenciad os campos "Advantages" e "Weaknesses"

--- src/components/pokemon/PokemonAdvantageWeakness.vue
+++ src/components/pokemon/PokemonAdvantageWeakness.vue
@@ -12,28 +12,38 @@
       <div class="d-flex flex-column ga-2">
         <h3 class="text-h5 font-weight-regular text-black">Advantages:</h3>
         <div class="d-flex ga-2 ml-4 mt-1 flex-wrap">
-          <span
-            v-for="advantage in infoPokemonStore.advantages"
-            :key="advantage"
-            class="px-2 py-1 rounded text-white"
-            :style="{ backgroundColor: getTypeColor(advantage) }"
-          >
-            {{ advantage }}
-          </span>
+          <template v-if="infoPokemonStore.advantages.length">
+            <span
+              v-for="advantage in infoPokemonStore.advantages"
+              :key="advantage"
+              class="px-2 py-1 rounded text-white"
+              :style="{ backgroundColor: getTypeColor(advantage) }"
+            >
+              {{ advantage }}
+            </span>
+          </template>
+          <template v-else>
+            <h4 class="text-primary">Advantages not found</h4>
+          </template>
         </div>
       </div>
 
       <div class="d-flex flex-column ga-2">
         <h3 class="text-h5 font-weight-regular text-black">Weaknesses:</h3>
         <div class="d-flex ga-2 ml-4 mt-1 flex-wrap">
-          <span
-            v-for="weakness in infoPokemonStore.weaknesses"
-            :key="weakness"
-            class="px-2 py-1 rounded text-white"
-            :style="{ backgroundColor: getTypeColor(weakness) }"
-          >
-            {{ weakness }}
-          </span>
+          <template v-if="infoPokemonStore.weaknesses.length">
+            <span
+              v-for="weakness in infoPokemonStore.weaknesses"
+              :key="weakness"
+              class="px-2 py-1 rounded text-white"
+              :style="{ backgroundColor: getTypeColor(weakness) }"
+            >
+              {{ weakness }}
+            </span>
+          </template>
+          <template v-else>
+            <h4 class="text-primary">Weaknesses not found</h4>
+          </template>
         </div>
       </div>
     </template>
